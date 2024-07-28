9ccd2984ba9fb95615ff404ba3da7ed405db5212
kourosh karimi <karimikorosh2000@gmail.com>
2024-07-28 17:07:21 +0330

    Villa Number Controller

--- MagicVilla_Web/Models/Dto/VillaNumberDTO.cs
+++ MagicVilla_Web/Models/Dto/VillaNumberDTO.cs
@@ -7,4 +7,5 @@ public class VillaNumberDTO
     [Required] public int VillaNo { get; set; }
     [Required] public int VillaID { get; set; }
     public string SpecialDetails { get; set; }
+    public VillaDTO Villa { get; set; }
 }
