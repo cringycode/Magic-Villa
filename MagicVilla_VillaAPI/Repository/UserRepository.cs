14a938fbe9019cce1215e387958d55922107105d
kourosh karimi <karimikorosh2000@gmail.com>
2024-07-31 13:45:53 +0330

    User & registeration repo

---
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepository;
+
+namespace MagicVilla_VillaAPI.Repository;
+
+public class UserRepository : IUserRepository
+{
+    #region DI
+
+    private readonly ApplicationDbContext _db;
+
+    public UserRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    #endregion
+
+    public bool IsUniqueUser(string username)
+    {
+        var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+        if (user is null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+    {
+        LocalUser user = new()
+        {
+            UserName = registerationRequestDTO.UserName,
+            Password = registerationRequestDTO.Password,
+            Name = registerationRequestDTO.Name,
+            Role = registerationRequestDTO.Role
+        };
+        _db.LocalUsers.Add(user);
+        await _db.SaveChangesAsync();
+        user.Password = "";
+        return user;
+    }
+}
