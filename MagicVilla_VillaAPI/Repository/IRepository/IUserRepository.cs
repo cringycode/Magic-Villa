0520c249fdff7300cd89502b4b8b72501844fa1e
kourosh karimi <karimikorosh2000@gmail.com>
2024-08-05 14:53:54 +0330

    User repo (Register)

--- MagicVilla_VillaAPI/Repository/IRepository/IUserRepository.cs
+++ MagicVilla_VillaAPI/Repository/IRepository/IUserRepository.cs
@@ -7,5 +7,5 @@ public interface IUserRepository
 {
     bool IsUniqueUser(string username);
     Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
-    Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO);
+    Task<UserDTO> Register(RegisterationRequestDTO registerationRequestDTO);
 }
